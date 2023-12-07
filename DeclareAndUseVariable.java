03c44bb629db306bf23192c49ef7f3af14ddec44
vandat3012 <luongvandat301220@gmail.com>
2023-12-07 15:34:36 +0700

    update bai tap

---
+++ DeclareAndUseVariable.java
@@ -0,0 +1,16 @@
+public class DeclareAndUseVariable {
+    public static void main(String[] args) {
+        int a = 1;
+        float b = 1.1f;
+        double c = 1.1;
+        boolean d = true;
+        char e = 'a';
+        String f = "Da Nang";
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+    }
+}
